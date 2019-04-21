a576c423b331fc17efdbdd0a8f2c53b74cd23969
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-04-21 17:46:58 -0600

    best advice

--- src/App.js
+++ src/App.js
@@ -1,28 +1,125 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { withData } from './DataProvider';
+import "./App.css"
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      advices: [],
+      toggle: true
+    }
+  }
+
+  componentWillMount(){
+    this.props.getAdvice()
+  }
+
+  handleChange = (e) => {
+    e.preventDefault()
+    this.setState({
+      search: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.getAdvice()
+  }
+  handleFav = (e) => {
+    e.preventDefault()
+    this.state.advices.push(this.props.advice)
+  }
+  handleToggle = () =>{
+    this.setState(prevState =>({
+        toggle: !prevState.toggle
+    }))
+}
+
   render() {
+    const styles={
+      btnParent:{
+        display: "flex",
+        flexDirection:"row",
+        justifyContent:"center",
+        marginTop: "1.3em",
+        marginBottom:"3.5em"
+      },
+      backParent:{
+        display: "flex",
+        flexDirection:"row",
+        justifyContent:"space-around",
+        marginTop: "1.3em",
+        marginBottom:"3.5em"
+      },
+      next:{
+        border:"red solid",
+        borderRadius: 5,
+        outline: "none",
+        marginLeft: '1%'
+      },
+      fav:{
+        border:"red solid",
+        borderRadius: 5,
+        outline: "none",
+        marginRight: '1%'    
+      },
+      favPage:{
+        border:"red solid",
+        borderRadius: 5,
+        outline: "none",        
+      },
+      advice:{
+        color: "black",
+        display:"block",
+        margin:"auto",
+        textAlign:"center",
+        marginTop:10,
+        fontSize: 35,
+        width: 350,
+      },
+      favs:{
+        color: "black",
+        display:"block",
+        margin:"auto",
+        width:"90%",
+        textAlign:"center",
+        marginTop:10,
+        fontSize: 35,
+        border: "solid green"
+      }
+    }
+    const mappedFavs = this.state.advices.map(advice => {
+        return(
+          <div>
+            <h1 style={styles.favs}>{advice}</h1>
+          </div>
+        )
+    })
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        {this.state.toggle ? 
+          <>
+            <div style={styles.btnParent}>
+              <button className='button' onClick={this.handleFav} style={styles.fav}>Save</button>
+              <button className='button' onClick={this.handleToggle} style={styles.favPage}>Favorites</button>
+              <button className='button' onClick={this.handleSubmit} style={styles.next}>Next</button>
+            </div>
+              <h1 style={styles.advice}>{this.props.advice}</h1>
+          </>
+          :
+          <>
+          <div style={styles.backParent}>
+          <button className='button' onClick={this.handleToggle} style={styles.favPage}>Back</button>
+          </div>
+          <div style={{overflow: "scroll", height: "70vh", width: 350, display: "block", margin:"auto"}}>
+            {mappedFavs}
+          </div>
+          </>
+        }
+      </>
     );
   }
 }
 
-export default App;
+export default withData(App);
---
+++ src/DataProvider.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import axios from "axios"
+const { Provider, Consumer } = React.createContext()
+
+class DataProvider extends Component {
+    constructor(){
+        super()
+        this.state={
+            advice: []
+        }
+    }
+    
+    getAdvice = ()=> {
+        axios.get(`https://api.adviceslip.com/advice`).then(res => {
+            this.setState({
+                advice: res.data.slip.advice
+            })
+        })
+    }
+
+    render() {
+        return (
+            <Provider value={{
+                getAdvice: this.getAdvice,
+                ...this.state
+            }}
+            >
+                {this.props.children}
+            </Provider>
+        );
+    }
+}
+
+export default DataProvider;
+
+export function withData(C) {
+    return props => <Consumer>{value => <C {...value}{...props} />}</Consumer>
+}
